cc9c15d867b21e23121ff3d82d13d91cb5a9c9ea
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-06-14 17:24:13 +0200

    Init Semantic UI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Grid, Header } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Grid>
+        <Grid.Row>
+          <Grid.Column>
+            <Header as='h1'>Lean Coffee</Header>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    )
   }
 }
 
-export default App;
+export default App
