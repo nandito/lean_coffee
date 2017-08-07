d6913f61c4f0ee540267f185a2ab0b9299351ea4
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-08-07 21:25:04 +0200

    Move Log in / log out to the Topmenu's AuthControl

--- src/LoginAuth0.jsx
+++ src/LoginAuth0.jsx
@@ -1,6 +1,8 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Auth0Lock from 'auth0-lock'
 import { withRouter } from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
 
 class LoginAuth0 extends Component {
 
@@ -10,12 +12,6 @@ class LoginAuth0 extends Component {
     this._lock = new Auth0Lock(props.clientId, props.domain)
   }
 
-  static propTypes = {
-    clientId: PropTypes.string.isRequired,
-    domain: PropTypes.string.isRequired,
-    history: PropTypes.object.isRequired,
-  }
-
   componentDidMount() {
     this._lock.on('authenticated', (authResult) => {
       window.localStorage.setItem('auth0IdToken', authResult.idToken)
@@ -23,22 +19,21 @@ class LoginAuth0 extends Component {
     })
   }
 
-  _showLogin = () => {
+  showLogin = () => {
     this._lock.show()
   }
 
   render() {
     return (
-      <div>
-        <span
-          onClick={this._showLogin}
-          className='dib pa3 white bg-blue dim pointer'
-        >
-          Log in with Auth0
-        </span>
-      </div>
+      <Button primary onClick={this.showLogin}>Log in</Button>
     )
   }
+}
+
+LoginAuth0.propTypes = {
+  clientId: PropTypes.string.isRequired,
+  domain: PropTypes.string.isRequired,
+  history: PropTypes.object.isRequired,
 }
 
 export default withRouter(LoginAuth0)
--- src/components/menu/AuthControl.jsx
+++ src/components/menu/AuthControl.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import { graphql, gql } from 'react-apollo'
 import { Button, Menu } from 'semantic-ui-react'
+import LoginAuth0 from '../../LoginAuth0'
+import { clientId, domain } from '../../App'
 
-const AuthControl = ({ auth }) => {
-  return (
-    <Menu.Menu position='right'>
-      <Menu.Item>
-        {/* { auth.isAuthenticated()
-          ? <Button onClick={() => auth.logout()}>Log out</Button>
-          : <Button primary onClick={() => auth.login()}>Log in</Button>
-        } */}
-      </Menu.Item>
-    </Menu.Menu>
-  )
+class AuthControl extends Component {
+  logout = () => {
+    // remove token from local storage and reload page to reset apollo client
+    window.localStorage.removeItem('auth0IdToken')
+    window.location.reload()
+  }
+
+  isLoggedIn = () => {
+    return this.props.data.user
+  }
+
+  render() {
+    return (
+      <Menu.Menu position='right'>
+        <Menu.Item>
+          { this.isLoggedIn()
+            ? <Button onClick={() => this.logout()}>Log out</Button>
+            : <LoginAuth0 clientId={clientId} domain={domain} />
+          }
+        </Menu.Item>
+      </Menu.Menu>
+    )
+  }
 }
 
-export default AuthControl
+const userQuery = gql`
+  query userQuery {
+    user {
+      id
+    }
+  }
+`
+
+export default graphql(userQuery, { options: {fetchPolicy: 'network-only' }})(withRouter(AuthControl))
