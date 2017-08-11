3ce05c863122cce30895256635947bdc6d569ea9
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-08-11 22:50:49 +0200

    Store username and id to the state when logged in

--- src/components/menu/AuthControl.jsx
+++ src/components/menu/AuthControl.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { Button, Menu } from 'semantic-ui-react'
 import LoginAuth0 from '../../LoginAuth0'
+import { logIn, logOut } from '../user/actions'
 import { clientId, domain } from '../../App'
 
 class AuthControl extends Component {
+  componentDidMount() {
+    if (this.props.data.user) {
+      const { data: { user }, logIn } = this.props
+
+      logIn(user)
+    }
+  }
+
   logout = () => {
+    this.props.logOut()
     window.localStorage.removeItem('auth0IdToken')
     window.location.reload()
   }
@@ -28,4 +39,11 @@ class AuthControl extends Component {
   }
 }
 
-export default withRouter(AuthControl)
+const mapDispatchToProps = (dispatch) => ({
+  logIn: ({ id, name }) => dispatch(logIn({ id, name })),
+  logOut: () => dispatch(logOut()),
+})
+
+const AuthControlWithRouter = withRouter(AuthControl)
+
+export default connect(null, mapDispatchToProps)(AuthControlWithRouter)
