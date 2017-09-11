e17b1048e1ecd3e83f461df38928a2f16a423ef7
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-09-11 14:10:24 +0200

    Move participants files to the pages folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 export { default as ListLeanCoffees } from './coffee/list/List'
 export { default as LeanCoffeeDetails } from './coffee/details/Details'
+
 export { default as Home } from './home/Home'
+
+export { default as ListParticipants } from './participant/List'
