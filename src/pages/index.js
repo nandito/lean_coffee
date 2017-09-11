1f391db5b88fedad473ffc513a6baf68c53dc196
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-09-11 14:06:54 +0200

    Move Home.jsx to the pages folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as ListLeanCoffees } from './coffee/list/List'
 export { default as LeanCoffeeDetails } from './coffee/details/Details'
+export { default as Home } from './home/Home'
