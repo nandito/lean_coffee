3bec6690d6e91a9fd3c38c1d687f0d522d42842e
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-09-15 12:13:49 +0200

    Initialize subscriptions
    * Install dependencies
    * Define subscription client
    * Update network interface

--- src/index.js
+++ src/index.js
@@ -5,8 +5,18 @@ import App from './App'
 // import registerServiceWorker from './registerServiceWorker'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
+import { addGraphQLSubscriptions } from 'add-graphql-subscriptions'
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
+
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj3okomy5fb9l0198ja30bnhu', {
+  reconnect: true,
+  timeout: 30000,
+  connectionParams: {
+    Authorization: `Bearer ${localStorage.getItem('auth0IdToken')}`,
+},
+})
 
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj3okomy5fb9l0198ja30bnhu'
@@ -26,8 +36,13 @@ networkInterface.use([{
   },
 }])
 
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+)
+
 const client = new ApolloClient({
-  networkInterface
+  networkInterface: networkInterfaceWithSubscriptions,
 })
 
 const store = createStore(
