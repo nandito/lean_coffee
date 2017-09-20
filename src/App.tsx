ee1ea225c64857aaac8db02c6bd874445a3cdf42
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-09-20 17:24:33 +0200

    Fix logged out user's authentication checking

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ class App extends React.Component<DefaultChildProps<AppProps, Response>, {}> {
       return <Loading />
     }
 
-    const isAuthenticated = typeof user !== 'undefined'
+    const isAuthenticated = typeof user !== 'undefined' && user !== null
     const userId = isAuthenticated && user.id
 
     return (
