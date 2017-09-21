157944008f618990c8aca9b7083a7aa8536e01c1
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-09-21 12:25:45 +0200

    Add Semantic UI's color types to the coffee constants

---
+++ src/pages/coffee/constants.ts
@@ -0,0 +1,17 @@
+import { SemanticCOLORS } from 'semantic-ui-react'
+
+const PURPLE: SemanticCOLORS = 'purple'
+const PINK: SemanticCOLORS = 'pink'
+const VIOLET: SemanticCOLORS = 'violet'
+
+export const COFFEE_STATE_COLORS = {
+  'TOPIC_COLLECTION': PURPLE,
+  'TOPIC_VOTING': PINK,
+  'DISCUSSION': VIOLET,
+}
+
+export const COFFEE_STATE_NAMES = {
+  'TOPIC_COLLECTION': 'Topic collection',
+  'TOPIC_VOTING': 'Topic voting',
+  'DISCUSSION': 'Discussion',
+}
