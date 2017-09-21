583f51acb218261b8b479a245eedef90c7ae1403
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-09-21 12:26:42 +0200

    Apply tslint rules on the coffee list page

--- src/pages/coffee/list/List.tsx
+++ src/pages/coffee/list/List.tsx
@@ -1,27 +1,35 @@
 import * as React from 'react'
-import { graphql } from 'react-apollo'
+import { DefaultChildProps, graphql } from 'react-apollo'
 import * as moment from 'moment'
-import { Card, Dimmer, Header, Label, Icon, Loader, Segment } from 'semantic-ui-react'
+import { Card, Header, Icon, Label, Segment } from 'semantic-ui-react'
+import { Loading } from '../../../components'
 import CreateLeanCoffee from './Create'
 import { COFFEE_STATE_NAMES, COFFEE_STATE_COLORS } from '../constants'
 import { getLeanCoffees } from '../../../graphql'
+import { getLeanCoffeesQuery } from '../../../schema'
 
-const ListLeanCoffees = ({ data }) => {
-  if (data.loading) return (
-    <Dimmer inverted active>
-      <Loader>Loading</Loader>
-    </Dimmer>
-  )
+export interface Props {
+  data: {
+    loading: boolean;
+    error: string;
+    allLeanCoffees: getLeanCoffeesQuery['allLeanCoffees'];
+  };
+}
 
-  if (data.error) {
-    console.error('Something went wrong:' , data.error)
+const ListLeanCoffees = ({data: {loading, error, allLeanCoffees}}: DefaultChildProps<Props, getLeanCoffeesQuery>) => {
+  if (loading) {
+    return <Loading />
+  }
+
+  if (error) {
+    console.error('Something went wrong:' , error)
     return null
   }
 
   return (
     <div>
-      <Header as='h2' icon textAlign='center'>
-        <Icon name='coffee' circular />
+      <Header as='h2' icon={true} textAlign='center'>
+        <Icon name='coffee' circular={true} />
         <Header.Content>
           Lean Coffees
         </Header.Content>
@@ -31,24 +39,24 @@ const ListLeanCoffees = ({ data }) => {
         <CreateLeanCoffee />
       </Segment>
 
-      <Card.Group itemsPerRow={3} stackable>
-        { data.allLeanCoffees.map((leanCoffee, index) =>
+      <Card.Group itemsPerRow={3} stackable={true}>
+        { allLeanCoffees.map((leanCoffee, index) =>
           <Card key={leanCoffee.id} href={`coffees/${leanCoffee.id}`} raised={index === 0}>
             <Card.Content>
-              { index === 0 && <Label color='red' corner='left' icon='birthday' /> }
+              {index === 0 && <Label color='red' corner='left' icon='birthday' />}
 
               <Label size='small' color={COFFEE_STATE_COLORS[leanCoffee.state]} ribbon='right'>
                 {COFFEE_STATE_NAMES[leanCoffee.state]}
               </Label>
 
               <Card.Header>
-                { moment(leanCoffee.createdAt).format('MMMM Do YYYY') }
+                {moment(leanCoffee.createdAt).format('MMMM Do YYYY')}
               </Card.Header>
               <Card.Meta>
                 hosted by: {leanCoffee.user ? leanCoffee.user.name : 'N/A'}
               </Card.Meta>
               <Card.Description>
-                { leanCoffee.topics.length
+                { leanCoffee.topics && leanCoffee.topics.length
                   ? leanCoffee.topics.map(topic => <Label size='mini' key={topic.id}>{topic.name}</Label>)
                   : <span>There are no topics specified</span>
                 }
@@ -61,8 +69,6 @@ const ListLeanCoffees = ({ data }) => {
   )
 }
 
-// ListLeanCoffees.propTypes = {
-//   data: PropTypes.object.isRequired,
-// }
-
-export default graphql(getLeanCoffees, { options: {fetchPolicy: 'network-only'}})(ListLeanCoffees)
+export default graphql<getLeanCoffeesQuery>(getLeanCoffees, {
+  options: { fetchPolicy: 'network-only' }
+})(ListLeanCoffees)
