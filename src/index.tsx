a0dcafe880e01e95282ede5f3e0be9f6830190cc
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-11-01 22:07:49 +0100

    Fix redux devtools config

--- src/index.tsx
+++ src/index.tsx
@@ -49,8 +49,6 @@ declare global {
   interface Window { __REDUX_DEVTOOLS_EXTENSION__: Function; }
 }
 
-window.__REDUX_DEVTOOLS_EXTENSION__ = window.__REDUX_DEVTOOLS_EXTENSION__ || {};
-
 const store = createStore(
   combineReducers({
     apollo: client.reducer(),
